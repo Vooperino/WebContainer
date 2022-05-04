d879a4e402d1f61621a9ddf7de3d1252ce04aed4
Voop <viperlv2@inbox.lv>
2022-05-04 20:52:18 +0300

    Updated Renew script

--- scripts/letsencrypt/renewAllCert.sh
+++ scripts/letsencrypt/renewAllCert.sh
@@ -3,8 +3,13 @@ checkDir() {
     [ -d "$@" ]
 }
 
+checkFile() {
+    [ -f "$@" ]
+}
+
 certStuffRoot="/web/cert_webroot"
 sslDirPath="/web/ssl"
+logFile="/scripts/letsencrypt/letsencrypt-renew.log"
 
 if ! checkDir $certStuffRoot; then
     echo "Creating cert_webroot folder"
@@ -14,6 +19,11 @@ fi
 if ! checkDir $sslDirPath; then
     echo "Creating ssl folder"
     mkdir -p $sslDirPath
+fi
+
+if ! checkFile $logFile; then
+    echo "Creating Log File"
+    touch $logFile
 fi
 
 /usr/bin/letsencrypt renew --config-dir $sslDirPath --webroot --webroot-path $certStuffRoot >> /scripts/letsencrypt/letsencrypt-renew.log
