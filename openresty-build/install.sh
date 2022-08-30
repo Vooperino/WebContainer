92afd5cd341b1dbd3d333607f1f94cac2094afe4
Voop <viperlv2@inbox.lv>
2022-08-30 16:10:57 +0300

    Updated OpenResty Install Script

--- openresty-build/install.sh
+++ openresty-build/install.sh
@@ -4,3 +4,18 @@ apt-get -y install software-properties-common
 add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
 apt-get update
 apt-get install openresty
+
+echo "Configuring Clean Install and Default Configuration!"
+
+echo "Updating Clean Install Dir"
+cp -r -f -v /temp_config/* /clean/config
+cp -r -f -v /temp_scripts/* /clean/scripts
+
+echo "Updating Config Dir"
+cp -r -f -v /temp_config/* /config/
+cp -r -f -v /temp_scripts/* /scripts
+
+cp -r -f -v /scripts/reloadWebSrv.sh /usr/bin/reloadWebSrv
+chmod 555 -R /usr/bin/reloadWebSrv
+
+echo "=== End of script ==="
