c0ffcad89f407eddabcf9e356584d8fc4162b3f8
Voop <viperlv2@inbox.lv>
2022-08-30 10:57:44 +0300

    Added install script

---
+++ nginx-build/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Installing Nginx!"
+
+apt-get update
+apt-get full-upgrade -y
+
+apt-get install nginx -y
+
+echo "Configuring Clean Install and Default Configuration!"
+
+echo "Updating Clean Install Dir"
+ls -la /temp_config
+cp -r -f -v /temp_config/* /clean/config
+
+echo "Updating Config Dir"
+cp -r -f -v /temp_config/* /config/
+
+echo "=== End of script ==="
