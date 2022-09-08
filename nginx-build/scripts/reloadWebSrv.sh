0ccf873fbdfd8bde1cc5cf3ff9839e0576f41915
Voop <viperlv2@inbox.lv>
2022-09-08 22:23:22 +0300

    Updated reloadWebSrv scripts for both

--- nginx-build/scripts/reloadWebSrv.sh
+++ nginx-build/scripts/reloadWebSrv.sh
@@ -3,10 +3,27 @@
 nginx_root="/etc/nginx"
 nginx_custom="/config/nginx"
 
+isEmptyDir() {
+    [ -n "$(find "$@" -maxdepth 0 -type d -empty 2>/dev/null)" ]
+}
+
+checkDir() {
+    [ -d "$@" ]
+}
+
 echo "Stopping Nginx Service"
 service nginx stop
 
 echo "Copying Configuration files"
+if isEmptyDir "/config/php"; then
+    output "Failed to validate php config directory. Copying defaults"
+    cp -r -f -v $CLEAN_PATH/config/php/* $php_custom
+fi
+
+if ! checkDir "/config/nginx"; then
+    output "Failed to validate nginx config directory. Copying defaults"
+    cp -r -f -v $CLEAN_PATH/config/nginx/ /config
+fi
 cp -r -f $nginx_custom/* $nginx_root
 
 echo "Starting Up Nginx Service"
--- openresty-build/scripts/reloadWebSrv.sh
+++ openresty-build/scripts/reloadWebSrv.sh
@@ -3,10 +3,28 @@
 openresty_root="/usr/local/openresty/nginx/conf"
 openresty_custom="/config/openresty"
 
+isEmptyDir() {
+    [ -n "$(find "$@" -maxdepth 0 -type d -empty 2>/dev/null)" ]
+}
+
+checkDir() {
+    [ -d "$@" ]
+}
+
 echo "Stopping OpenResty Service"
 sysctl stop openresty
 
 echo "Copying Configuration Files"
+if ! checkDir "/config/openresty"; then
+    output "Failed to validate openresty config directory. Copying defaults"
+    mkdir -p /config/openresty
+    cp -r -f -v $CLEAN_PATH/config/openresty/ /config
+fi
+
+if isEmptyDir "/config/openresty"; then
+    output "Failed to validate openresty config directory. Copying defaults"
+    cp -r -f -v $CLEAN_PATH/config/openresty/ /config
+fi
 rm -rf $openresty_root/*
 cp -r -f -v $openresty_custom/* $openresty_root
 
