78318a2f6988626c87581c312ba4f6825e308eb1
Voop <viperlv2@inbox.lv>
2022-09-08 22:22:51 +0300

    Updated Init for openresty

--- openresty-build/init.sh
+++ openresty-build/init.sh
@@ -66,6 +66,12 @@ fi
 
 if ! checkDir "/config/openresty"; then
     output "Failed to validate openresty config directory. Copying defaults"
+    mkdir -p /config/openresty
+    cp -r -f -v $CLEAN_PATH/config/openresty/ /config
+fi
+
+if isEmptyDir "/config/openresty"; then
+    output "Failed to validate openresty config directory. Copying defaults"
     cp -r -f -v $CLEAN_PATH/config/openresty/ /config
 fi
 
