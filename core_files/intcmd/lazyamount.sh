39b6576c4ecd7be74775c9bd8def5980771e3250
Voop <voop@voop.lv>
2025-09-14 14:14:34 +0300

    Added new commands

---
+++ core_files/intcmd/lazyamount.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+LOCK_FILE="/tmp/lazymount.lock"
+
+if [[ -f "$LOCK_FILE" ]]; then
+    echo "[ERROR] Lazy mount script is already executed. Exiting to prevent multiple executions. (This is only supposed to run once at container start)"
+    exit 1
+else
+    echo "[INFO] Creating lock file to prevent multiple executions..."
+    touch "$LOCK_FILE"
+    if [[ $? -ne 0 ]]; then
+        echo "[ERROR] Failed to create lock file: $LOCK_FILE"
+        exit 1
+    fi
+fi
+
+declare -A MOUNTS
+
+MOUNTS["/usr/local/openresty/nginx/conf"]="/config/openresty"
+MOUNTS["/etc/php"]="/config/php"
+
+for to_path in "${!MOUNTS[@]}"; do
+  set -Eeuo pipefail
+  from_path="${MOUNTS[${to_path}]}"
+  rm -rf "${to_path}"
+  if [ ! -d "$from_path" ]; then
+    echo "[INFO] Creating Direcotory '$from_path' and applying permissions"
+    mkdir -vp "$from_path"
+  fi
+  chmod -R 777 "$from_path"
+  mkdir -vp "$(dirname "${to_path}")"
+  ln -sT "${from_path}" "${to_path}"
+  echo Mounted $(basename "${from_path}")
+done
