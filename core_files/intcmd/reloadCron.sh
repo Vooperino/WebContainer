07c606c8cf24317641b8627091a5e527e779f995
Voop <voop@voop.lv>
2025-09-14 16:02:32 +0300

    Updated Cron Reload Command

--- core_files/intcmd/reloadCron.sh
+++ core_files/intcmd/reloadCron.sh
@@ -7,4 +7,24 @@ if [[ ! -f "$SUPERVISOR_CONFIG" ]]; then
     exit 1
 fi
 
-supervisorctl -c "$SUPERVISOR_CONFIG" restart supercronic
+SERVICE_NAME="supercronic"
+SERVICE=$(supervisorctl status "${SERVICE_NAME}" | awk '{print $2}')
+
+if [ "$SERVICE" == "RUNNING" ]; then
+    echo "[INFO] Stopping $SERVICE_NAME..."
+    supervisorctl stop "${SERVICE_NAME}"
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] Failed to stop $SERVICE_NAME."
+        exit 1
+    fi
+    echo "[INFO] $SERVICE_NAME stopped successfully."
+fi
+
+echo "[INFO] Starting $SERVICE_NAME..."
+supervisorctl start "${SERVICE_NAME}"
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to start $SERVICE_NAME."
+    exit 1
+fi
+echo "[INFO] $SERVICE_NAME started successfully."
+exit 0
