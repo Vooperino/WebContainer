292a3723f5eb19fd56ba6e9c655a08185fb83a98
Voop <voop@voop.lv>
2025-09-14 13:15:31 +0300

    Adding applypermission command as background runner

---
+++ core_files/intcmd/applypermissions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+WWDATA_PERMISSION_CODE=0755
+WWWDATA_PATHS=("/run/php")
+
+while true; do
+    for path in "${WWWDATA_PATHS[@]}"; do
+        if [[ -d "$path" ]]; then
+            echo "[INFO] Setting permissions for $path to $WWDATA_PERMISSION_CODE"
+            chmod -R "$WWDATA_PERMISSION_CODE" "$path"
+            if [[ $? -ne 0 ]]; then
+                echo "[ERROR] Failed to set permissions for $path"
+            else
+                echo "[INFO] Permissions for $path set successfully."
+            fi
+        else
+            echo "[WARNING] Path $path does not exist, skipping."
+        fi
+    done
+    echo "[INFO] Permission fix completed. Sleeping for 5 seconds."
+    sleep 5
+done
